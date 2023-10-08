5efaab2a295b7eadf2da7617edf8b4737860009c
AmidTit <114725214+AmidTit@users.noreply.github.com>
2023-10-09 03:33:04 +1100

    first commit

---
+++ Package/TestPackageTask.py
@@ -0,0 +1,45 @@
+import unittest
+from test_task import get_file, do_replacement
+
+
+class TestPackageTask(unittest.TestCase):
+
+    def test_get_file_type(self):
+        data_url = "https://api.github.com/repos/thewhitesoft/student-2023-assignment/contents/data.json"
+        self.assertEqual(get_file(data_url)['type'], "file")
+
+    def test_get_file_encoding(self):
+        data_url = "https://api.github.com/repos/thewhitesoft/student-2023-assignment/contents/data.json"
+        self.assertEqual(get_file(data_url)['encoding'], "base64")
+
+    def test_get_file_with_empty_url(self):
+        data_url = "https://api.github.com//"
+        self.assertEqual(get_file(data_url), "No file")
+
+    def test_do_replacement(self):
+        data = ["Hello wtf"]
+        replacement = [{"replacement": "wtf", "source": "World"}]
+
+        self.assertEqual(do_replacement(replacement, data), ["Hello World"])
+
+    def test_do_replacement_with_None(self):
+        data = ["Hello wtf", "Looks like I hacked you buddy"]
+        replacement = [
+            {"replacement": "wtf", "source": "World"},
+            {"replacement": "Looks like I hacked you buddy", "source": None}
+                        ]
+        self.assertEqual(do_replacement(replacement, data), ["Hello World"])
+
+    def test_do_replacement_with_repetition(self):
+        data = ["Hello wtf", "Looks like I hacked you buddy", "Let`s rock this srgrbbxf546"]
+        replacement = [
+            {"replacement": "wtf", "source": "Girls and Boys"},
+            {"replacement": "Looks like I hacked you buddy", "source": None},
+            {"replacement": "wtf", "source": "World"},
+            {"replacement": "srgrbbxf546", "source": "dump"}
+        ]
+
+        self.assertEqual(do_replacement(replacement, data), ["Hello World", "Let`s rock this dump"])
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Package/test_task.py
@@ -0,0 +1,65 @@
+import requests
+import json
+import base64
+import user_data
+
+
+def get_file(file_url):
+    response = requests.get(file_url, auth=user_data.auth)
+    data = response.text
+
+    if response.status_code == 200:
+        return json.loads(data)
+    else:
+        return "No file"
+
+
+
+def decode_file(json_data):
+    decoded_bytes = base64.b64decode(json_data["content"])
+    decoded_str = decoded_bytes.decode("ascii")
+
+    return json.loads(decoded_str)
+
+
+def upload_file(file_name, uploadable_file):
+    with open(file_name + ".json", "w") as file:
+        json.dump(uploadable_file, file, indent=2)
+
+
+def do_replacement(replacements_json, data_json):
+    none_set = set()
+
+    for i in range(-1, -len(replacements_json) - 1, -1):
+
+        replacement = replacements_json[i]["replacement"]
+        source = replacements_json[i]["source"]
+
+        for j in range(len(data_json)):
+
+            string = data_json[j]
+
+            if replacement in string and source != None:
+
+                changed_string = string.replace(replacement, source)
+                data_json[j] = data_json[j].replace(string, changed_string)
+
+            elif replacement in string and source == None:
+
+                none_set.add(string)
+
+    return [item for item in data_json if item not in none_set]
+
+
+# Точка входа в программу
+if __name__ == "__main__":
+    data_url = "https://api.github.com/repos/thewhitesoft/student-2023-assignment/contents/data.json"
+    data_json = decode_file(get_file(data_url))
+    upload_file("data", data_json)
+
+    replacements_url = "https://api.github.com/repos/thewhitesoft/student-2023-assignment/contents/replacement.json"
+    replacements_json = decode_file(get_file(replacements_url))
+    upload_file("replacement", replacements_json)
+
+    result = do_replacement(replacements_json, data_json)
+    upload_file("result", result)
